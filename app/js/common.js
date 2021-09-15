c131eb5f71f0176d8e0b56b84af0db7727937309
Viktoria <zaharova181114@gmail.com>
2021-09-15 12:52:54 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,19 @@
+$('.btn-burger').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeOut();
+});
+
+// активная ссылка меню
+$('.menu-brands a').each(function () {
+    let location = window.location.href;
+    let link = this.href;
+    if (location === link) {
+        $(this).addClass('active');
+    }
+});
+// end
