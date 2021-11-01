fd084b22ba296572344bc91fa38728b400049b71
Viktoria <zaharova181114@gmail.com>
2021-11-01 22:16:08 +0200

    edit1-1

--- app/js/common.js
+++ app/js/common.js
@@ -24,3 +24,11 @@ $('.tooltip-box').tooltip();
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
 new WOW().init();
+
+$(document).mouseup(function (e) { // событие клика по веб-документу
+    var div = $(".mobile-menu"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        div.fadeOut(); // скрываем его
+    }
+});
