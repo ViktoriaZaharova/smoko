cb7a556d0f3171ee98610202a75517007bf31a8d
Viktoria <zaharova181114@gmail.com>
2021-11-19 23:30:02 +0200

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,24 @@
 $('.btn-burger').on('click', function (e) {
     e.preventDefault();
     $('.mobile-menu').fadeToggle();
+    $('.overlay').fadeToggle();
 });
 
 $('.btn-close, .links-edit').on('click', function (e) {
     e.preventDefault();
     $('.mobile-menu').fadeOut();
+    $('.overlay').fadeOut();
 });
+
+$(document).mouseup(function (e){ // событие клика по веб-документу
+    var div = $(".mobile-menu"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        div.hide(300); // скрываем его
+        $('.overlay').fadeOut();
+    }
+});
+
 
 // активная ссылка меню
 $('.menu-brands a').each(function () {
@@ -28,20 +40,35 @@ $('.label-toggle').click(function() {
     var element = $('.toggle-input');
 
     if(element.is(':checked')) {
-        $('.flavor-card__img').fadeOut();
-    } else {
         $('.flavor-card__img').css('display', 'flex');
+    } else {
+        $('.flavor-card__img').fadeOut();
     }
 });
 
 
 new WOW().init();
 
-$(document).mouseup(function (e) { // событие клика по веб-документу
-    var div = $(".mobile-menu"); // тут указываем ID элемента
-    if (!div.is(e.target) // если клик был не по нашему блоку
-        && div.has(e.target).length === 0) { // и не по его дочерним элементам
-        div.fadeOut(); // скрываем его
+$(document).ready(function () {
+    var heightBlock = $('.box-text-hidden').height();
+    if (heightBlock > 70) {
+        $('.box-text-hidden').addClass('hidden-block');
+        // $(this).find('.equipment-product').slice(12).hide();
+        $('.box-text-hidden').append('<a href="#" class="color-accent read-view">читать далее</a>');
     }
 });
+
+$('.read-view').on('click', function (e) {
+    e.preventDefault();
+    $('.box-text-hidden').slideDown();
+
+    var onBlock = $('.box-text-hidden').height();
+    if(onBlock <= 0) {
+        $('.read-view').hide();
+    }
+});
+
+
+
+
 
